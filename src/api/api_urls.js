ffa73e4c874bdd297cb494ccb37ced4e7bc9b014
manoharlikhit <sesettikalyan@gmail.com>
2024-10-08 21:39:09 +0530

    baseUrl changed

--- src/api/api_urls.js
+++ src/api/api_urls.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  baseUrl: "https://bms-backend-xi.vercel.app/",
+  baseUrl: "https://bms-backend-xi.vercel.app",
   testUrl: "http://localhost:4000",
   isTestServer: false,
 });
